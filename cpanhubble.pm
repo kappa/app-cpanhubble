8da717f109860d37c9fa206e58a200667e57fe49
Alex Kapranoff <kapranoff@gmail.com>
2010-04-02 16:02:57 +0400

    First shot at actual web iface

--- cpanhubble.pm
+++ cpanhubble.pm
@@ -1,8 +1,36 @@
 package cpanhubble;
 use Dancer;
 
+use lib './lib';
+use CpanHub;
+use AnyEvent;
+
 get '/' => sub {
     template 'index';
+};
+
+get '/search' => sub {
+    my ($cpan, $gh);
+
+    my $cv = AnyEvent->condvar;
+    $cv->begin;
+    cpan_search_req(params->{q}, sub {
+            $cpan = $_[0];
+            $cv->end;
+        });
+
+    $cv->begin;
+    github_search_req(params->{q}, sub {
+            $gh = $_[0];
+            $cv->end;
+        });
+
+    $cv->recv;
+
+    my $res =
+        merge_cpan_and_github($cpan, $gh);
+
+    template 'serp', { res => $res, query => params->{q} };
 };
 
 true;
